cca6056cad10d23134ea84f1768bdb0cb22bd87c
YongJunXi <ernestyongjunxi@gmail.com>
2025-08-18 13:32:05 +0800

    Pass lambda methods to issue card children and render contents by flex

--- src/app/shared/card-view/card-view.component.ts
+++ src/app/shared/card-view/card-view.component.ts
@@ -1,7 +1,6 @@
 import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { IssueService } from '../../core/services/issue.service';
 import { IssuesDataTable } from '../issue-tables/IssuesDataTable';
-import { ACTION_BUTTONS } from '../issue-tables/issue-tables.component';
 import { MatSort, Sort } from '@angular/material/sort';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { TableSettings } from '../../core/models/table-settings.model';
@@ -13,8 +12,20 @@ import { IssueTableSettingsService } from '../../core/services/issue-table-setti
 import { PhaseService } from '../../core/services/phase.service';
 import { LoggingService } from '../../core/services/logging.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Issue } from '../../core/models/issue.model';
+import { Issue, STATUS } from '../../core/models/issue.model';
 import { Observable } from 'rxjs';
+import { UndoActionComponent } from '../action-toasters/undo-action/undo-action.component';
+import { finalize } from 'rxjs/operators';
+
+export enum ACTION_BUTTONS {
+  VIEW_IN_WEB,
+  MARK_AS_RESPONDED,
+  MARK_AS_PENDING,
+  RESPOND_TO_ISSUE,
+  FIX_ISSUE,
+  DELETE_ISSUE,
+  RESTORE_ISSUE
+}
 
 @Component({
   selector: 'app-card-view',
@@ -66,6 +77,10 @@ export class CardViewComponent implements OnInit, AfterViewInit {
     });
   }
 
+  globalTableIndex(localTableIndex: number) {
+    return this.issues.getGlobalTableIndex(localTableIndex);
+  }
+
   sortChange(newSort: Sort) {
     this.tableSettings.sortActiveId = newSort.active;
     this.tableSettings.sortDirection = newSort.direction;
@@ -76,5 +91,162 @@ export class CardViewComponent implements OnInit, AfterViewInit {
     this.tableSettings.pageSize = pageEvent.pageSize;
     this.tableSettings.pageIndex = pageEvent.pageIndex;
     this.issueTableSettingsService.setTableSettings(this.table_name, this.tableSettings);
+  }
+
+  isActionVisible(action: ACTION_BUTTONS): boolean {
+    return this.actions.includes(action);
+  }
+
+  markAsResponded(issue: Issue, event: Event) {
+    this.logger.info(`IssueTablesComponent: Marking Issue ${issue.id} as Responded`);
+    const newIssue = issue.clone(this.phaseService.currentPhase);
+    newIssue.status = STATUS.Done;
+    this.issueService.updateIssue(newIssue).subscribe(
+      (updatedIssue) => {
+        this.issueService.updateLocalStore(updatedIssue);
+      },
+      (error) => {
+        this.errorHandlingService.handleError(error);
+      }
+    );
+    event.stopPropagation();
+  }
+
+  isResponseEditable() {
+    return this.permissions.isTeamResponseEditable() || this.permissions.isTesterResponseEditable();
+  }
+
+  markAsPending(issue: Issue, event: Event) {
+    this.logger.info(`IssueTablesComponent: Marking Issue ${issue.id} as Pending`);
+    const newIssue = issue.clone(this.phaseService.currentPhase);
+    newIssue.status = STATUS.Incomplete;
+    this.issueService.updateIssue(newIssue).subscribe(
+      (updatedIssue) => {
+        this.issueService.updateLocalStore(updatedIssue);
+      },
+      (error) => {
+        this.errorHandlingService.handleError(error);
+      }
+    );
+    event.stopPropagation();
+  }
+
+  logIssueRespondRouting(id: number) {
+    this.logger.info(`IssueTablesComponent: Proceeding to Respond to Issue ${id}`);
+  }
+
+  logIssueEditRouting(id: number) {
+    this.logger.info(`IssueTablesComponent: Proceeding to Edit Issue ${id}`);
+  }
+
+  /**
+   * Gets the number of resolved disputes.
+   */
+  todoFinished(issue: Issue): number {
+    return issue.issueDisputes.length - issue.numOfUnresolvedDisputes();
+  }
+
+  /**
+   * Checks if all the disputes are resolved.
+   */
+  isTodoListChecked(issue: Issue): boolean {
+    return issue.issueDisputes && issue.numOfUnresolvedDisputes() === 0;
+  }
+
+  viewIssueInBrowser(id: number, event: Event) {
+    this.logger.info(`IssueTablesComponent: Opening Issue ${id} on Github`);
+    this.githubService.viewIssueInBrowser(id, event);
+  }
+
+  deleteOrRestoreIssue = (isDeleteAction: boolean, id: number, event: Event, actionUndoable: boolean = true) => {
+    const deletingKeyword = 'Deleting';
+    const undeletingKeyword = 'Undeleting';
+    this.logger.info(`IssueTablesComponent: ${isDeleteAction ? deletingKeyword : undeletingKeyword} Issue ${id}`);
+
+    this.issuesPendingAction = { ...this.issuesPendingAction, [id]: true };
+
+    let observableActionedIssue: Observable<Issue>;
+    if (isDeleteAction) {
+      observableActionedIssue = this.issueService.deleteIssue(id);
+    } else {
+      observableActionedIssue = this.issueService.undeleteIssue(id);
+    }
+    observableActionedIssue
+      .pipe(
+        finalize(() => {
+          const { [id]: issueDeletedOrRestored, ...theRest } = this.issuesPendingAction;
+          this.issuesPendingAction = theRest;
+        })
+      )
+      .subscribe(
+        () => this.handleIssueActionPerformedSuccess(isDeleteAction, id, event, actionUndoable),
+        (error) => this.errorHandlingService.handleError(error)
+      );
+    event.stopPropagation();
+  };
+
+  private handleIssueActionPerformedSuccess(isDeleteAction: boolean, id: number, event: Event, actionUndoable: boolean) {
+    const deletedKeyword = 'Deleted';
+    const restoredKeyword = 'Restored';
+    if (!actionUndoable) {
+      return;
+    }
+    let snackBarRef = null;
+    snackBarRef = this.snackBar.openFromComponent(UndoActionComponent, {
+      data: { message: `${isDeleteAction ? deletedKeyword : restoredKeyword} issue ${id}` },
+      duration: this.snackBarAutoCloseTime
+    });
+    snackBarRef.onAction().subscribe(() => {
+      this.deleteOrRestoreIssue(!isDeleteAction, id, event, false);
+    });
+  }
+
+  isActionPerformAllowed = (isDeleteAction: boolean, id: number) => {
+    const actionButton = isDeleteAction ? this.action_buttons.DELETE_ISSUE : this.action_buttons.RESTORE_ISSUE;
+    const isPermissionGranted = this.isIssueActionPermitted(isDeleteAction);
+    return isPermissionGranted && !this.issuesPendingAction[id] && this.isActionVisible(actionButton);
+  };
+
+  private isIssueActionPermitted(isDeleteAction: boolean) {
+    if (isDeleteAction) {
+      return this.permissions.isIssueDeletable();
+    }
+    return this.permissions.isIssueRestorable();
+  }
+
+  shouldEnablePendingButton = () => {
+    return (
+      (this.userService.currentUser.role === 'Student' || this.userService.currentUser.role === 'Admin') &&
+      this.isActionVisible(this.action_buttons.MARK_AS_PENDING)
+    );
+  };
+
+  shouldEnablePendingActionSpinner = (id: number) => {
+    return (
+      this.issuesPendingAction[id] &&
+      (this.isActionVisible(this.action_buttons.DELETE_ISSUE) || this.isActionVisible(this.action_buttons.RESTORE_ISSUE))
+    );
+  };
+
+  shouldEnableRespondToIssue = (issue: Issue): boolean => {
+    return this.isResponseEditable() && !issue.status && this.isActionVisible(this.action_buttons.RESPOND_TO_ISSUE);
+  };
+
+  shouldEnableMarkAsResponded = (issue: Issue): boolean => {
+    return this.isResponseEditable() && issue.status && this.isActionVisible(this.action_buttons.MARK_AS_RESPONDED);
+  };
+
+  shouldEnableEditIssue = () => {
+    return this.permissions.isIssueEditable() && this.isActionVisible(this.action_buttons.FIX_ISSUE);
+  };
+
+  onActionButtonClick(action: ACTION_BUTTONS, issueId: number, event: Event) {
+    switch (action) {
+      case ACTION_BUTTONS.VIEW_IN_WEB:
+        this.viewIssueInBrowser(issueId, event);
+        break;
+      default:
+        break;
+    }
   }
 }
--- src/app/shared/issue-card/issue-card.component.ts
+++ src/app/shared/issue-card/issue-card.component.ts
@@ -1,6 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Issue } from '../../core/models/issue.model';
 import { LabelService } from '../../core/services/label.service';
+import { ACTION_BUTTONS } from '../card-view/card-view.component';
+import { PermissionService } from '../../core/services/permission.service';
+import { LoggingService } from '../../core/services/logging.service';
 
 @Component({
   selector: 'app-issue-card',
@@ -9,6 +12,33 @@ import { LabelService } from '../../core/services/label.service';
 })
 export class IssueCardComponent {
   @Input() issue: Issue;
+  @Input() actions: ACTION_BUTTONS[];
+  @Input() isActionPerformAllowed!: (flag: boolean, id: number) => boolean;
+  @Input() shouldEnablePendingActionSpinner!: (id: number) => boolean;
+  @Input() deleteOrRestoreIssue!: (flag: boolean, id: number) => void;
+  @Input() shouldEnableMarkAsResponded!: (issue: Issue) => boolean;
+  @Input() shouldEnableEditIssue!: () => boolean;
+  @Input() shouldEnableRespondToIssue!: (issue: Issue) => boolean;
+  @Input() shouldEnablePendingButton!: () => boolean;
 
-  constructor(private labelService: LabelService) {}
+  public readonly action_buttons = ACTION_BUTTONS;
+
+  @Output() actionButtonClicked: EventEmitter<IssueActionEvent> = new EventEmitter();
+
+  constructor(private labelService: LabelService, public permissions: PermissionService, private logger: LoggingService) {}
+
+  isActionVisible(action: ACTION_BUTTONS): boolean {
+    return this.actions.includes(action);
+  }
+
+  onActionButtonClick(action: ACTION_BUTTONS, event: Event): void {
+    this.actionButtonClicked.emit({ action: action, issueId: this.issue.id, event: event });
+    this.logger.info(`Action ${action} clicked for issue ${this.issue.id}`);
+  }
+}
+
+interface IssueActionEvent {
+  action: ACTION_BUTTONS;
+  issueId: number;
+  event: Event;
 }
